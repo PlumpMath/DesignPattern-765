35250f7a304d1d72d27473f296c979a61d0a9839
talkin <taojing1106@163.com>
2014-12-21 22:51:27 +0800

    Add DeractorPattern

---
+++ src/main/java/com/tw/DeractorPattern/Rectangle.java
@@ -0,0 +1,11 @@
+package com.tw.DeractorPattern;
+
+/**
+ * Created by jtao on 12/18/14.
+ */
+public class Rectangle implements Shape {
+    @Override
+    public String draw() {
+        return "Drawing rectangle";
+    }
+}
---
+++ src/main/java/com/tw/DeractorPattern/Shape.java
@@ -0,0 +1,8 @@
+package com.tw.DeractorPattern;
+
+/**
+ * Created by jtao on 12/18/14.
+ */
+public interface Shape {
+    String draw();
+}
